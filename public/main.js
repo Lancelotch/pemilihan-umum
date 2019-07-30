3361a2936aa6ba3ff71a86cba21f63f3f88bc7da
Candra Darmawan <chandra.souvenir@gmail.com>
2019-07-30 16:36:41 +0700

    add chart and dataPoint on it using chartJS

--- public/main.js
+++ public/main.js
@@ -15,3 +15,28 @@ form.addEventListener("submit", e => {
     .catch(error => console.log(error));
   e.preventDefault();
 });
+
+let dataPoints = [
+  { label: "Pulau", y: 0 },
+  { label: "Gunung", y: 0 },
+  { label: "Pantai", y: 0 },
+  { label: "Taman", y: 0 }
+];
+
+const chartContainer = document.querySelector("#chart-container");
+if (chartContainer) {
+  const chart = new CanvasJS.Chart("chart-container", {
+    animationEnabled: true,
+    theme: "theme1",
+    title: {
+      text: "Voting Result"
+    },
+    data: [
+      {
+        type: "column",
+        dataPoints: dataPoints
+      }
+    ]
+  });
+  chart.render();
+}
