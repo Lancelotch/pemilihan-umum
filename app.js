1c3044da075cc95a5f99b6c2e604a34e834072c3
Candra Darmawan <chandra.souvenir@gmail.com>
2019-07-30 14:54:44 +0700

    Add * Init depedency  - pusher  - express  - body-parser  - cors  - mongoose * Create routes polling * create http request for integrate push * create simple UI for polling

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const polling = require('./routes/polling');
+
+const app = express();
+
+//Set public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+//Enable CORS
+app.use(cors());
+
+app.use('/polling', polling);
+
+const port = 5000;
+
+app.listen(port, ()=> {
+    console.log(`Server has started from port : ${port}`);
+})
---
+++ routes/polling.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+var Pusher = require("pusher");
+
+var channels_client = new Pusher({
+  appId: "832907",
+  key: "a7f4319adef7c6a424fb",
+  secret: "e7dbd34c6b96ff49aab9",
+  cluster: "mt1",
+  encrypted: true
+});
+
+router.get("/", (req, res) => {
+  res.send("Sip dah jalan-jalan");
+});
+
+router.post("/", (req, res) => {
+  channels_client.trigger("location-polling", "location-vote", {
+    points: 1,
+    location: req.body.location
+  });
+  return res.status(200).json({success: true, message: "Terimakasih atas pemilihan suaranya"})
+});
+
+module.exports = router;
